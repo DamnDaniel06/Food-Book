3b480c30ea63838f7d9a9507e49f1aedc0fa693c
Daniel Tshipuk <ST10121741@vcconnect.edu.za>
2024-04-05 13:13:39 +0200

    Add project files.

---
+++ Food-Book/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there. Welcome to Dig-in");
+
+            Console.WriteLine("Type [W] to write a new recipe or [L] to list the current available recepts");
+            string input1 = Console.ReadLine();
+        }
+    }
+}
