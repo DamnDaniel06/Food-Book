e3b7349fe7d0650252588aea23277e83b7416f8a
DamnDaniel06 <162707616+DamnDaniel06@users.noreply.github.com>
2024-05-20 13:45:21 +0200

    Created the "Display" and "Add" classes.

---
+++ Food-Book/Display.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food_Book
+{
+    public class Display
+    {
+        public void DisplayRecipe(Recipe recipe, int x)
+        {
+            Console.WriteLine($"**************************************************\n" +
+                $"Recipe name: {recipe.Name}\n" +
+                $"Number of Ingredients:    {recipe.ingediants.Count}\n" +
+                $"Number of Steps:          {recipe.steps.Count}\n" +
+                "\t\tINGREDIENTS\t\t\n" +
+                "-----------------------");
+
+            foreach (var ing in recipe.ingediants)
+            {
+                Console.WriteLine(
+                    $"Name:                 {ing.name}\n" +
+                    $"Unit of Measurement:  {ing.UOM}\n" +
+                    $"Quantity:             {ing.Quantity * x}\n" +
+                    $"----");
+            }
+            Console.WriteLine("\t\tSTEPS\t\t\n" +
+                              "--------------");
+            foreach (var step in recipe.steps)
+            {
+                Console.WriteLine($"Step {step.step}\n" +
+                    $"{step.instruction}\n" +
+                    $"--");
+            }
+        }
+        static void DisplayRecipe(Recipe recipe, double x)
+        {
+            Console.WriteLine($"**************************************************\n" +
+                $"Recipe name: {recipe.Name}\n" +
+                $"Number of Ingredients:    {recipe.ingediants.Count}\n" +
+                $"Number of Steps:          {recipe.steps.Count}\n" +
+                "       INGREDIENTS     \n" +
+                "-----------------------");
+
+            foreach (var ing in recipe.ingediants)
+            {
+                Console.WriteLine(
+                    $"Name:                 {ing.name}\n" +
+                    $"Unit of Measurement:  {ing.UOM}\n" +
+                    $"Quantity:             {ing.Quantity * x}\n" +
+                    $"----");
+            }
+            Console.WriteLine("     STEPS   \n" +
+                              "--------------");
+            foreach (var step in recipe.steps)
+            {
+                Console.WriteLine($"Step {step.step}\n" +
+                    $"{step.instruction}\n" +
+                    $"--");
+            }
+        }
+    }
+}
