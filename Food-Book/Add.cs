a4e8230c0988d8e15288839edbf1cdd7c45d85f0
DamnDaniel06 <162707616+DamnDaniel06@users.noreply.github.com>
2024-05-23 13:56:33 +0200

    Updated the Add class to exit if certain condition are not met. This occurs when that input is not correct at least 3 time, triggering the end of the loop in the class which will mark the end of the class

--- Food-Book/Add.cs
+++ Food-Book/Add.cs
@@ -14,168 +14,193 @@ namespace Food_Book
         /// <returns></returns>
         public Recipe AddRecipe()
         {
-            
-
             Console.WriteLine("-----");
             Recipe recipe = new Recipe();
+            bool isWorking = true;
 
-            bool isValid = true ;
-            int counter = 0;
-
-            Console.WriteLine("Please enter the Name of Recipe");
-            string name = Console.ReadLine();
-            isValid = InputChecker.StringChecker(name);
-            while(isValid==false&& counter < 2)
+            while (isWorking == true)
             {
-                Console.WriteLine("Please Try again to enter the recipe name");
+                
+
+                bool isValid = true;
+                int counter = 0;
+
+                Console.WriteLine("Please enter the Name of Recipe");
+                string name = Console.ReadLine();
                 isValid = InputChecker.StringChecker(name);
-                counter++;
-            }
-            if (isValid == false)
-            {
-                //break;
-            }
-            counter = 0;
-            recipe.Name = name;
-            
+                while (isValid == false && counter < 2)
+                {
+                    Console.WriteLine("Please Try again to enter the recipe name");
+                    isValid = InputChecker.StringChecker(name);
+                    counter++;
+                }
+                if (isValid == false)
+                {
+                    isWorking = false;
+                    break;
+                }
+                else
+                {
+                    counter = 0;
+                    recipe.Name = name;
+                }
+                
 
 
-            Console.WriteLine("Please enter the number of ingredients");
-            string Ingrd = Console.ReadLine();
-            isValid = InputChecker.IntChecker(Ingrd);
-            while (isValid == false && counter < 2)
-            {
-                Console.WriteLine("Please Try again to enter the number of ingredients");
+
+                Console.WriteLine("Please enter the number of ingredients");
+                string Ingrd = Console.ReadLine();
                 isValid = InputChecker.IntChecker(Ingrd);
-                counter++;
-            }
-            if (isValid == false)
-            {
-                //break;
-            }
-            counter = 0;
-            int numIngrd = System.Convert.ToInt32(Ingrd);
+                while (isValid == false && counter < 2)
+                {
+                    Console.WriteLine("Please Try again to enter the number of ingredients");
+                    isValid = InputChecker.IntChecker(Ingrd);
+                    counter++;
+                }
+                if (isValid == false)
+                {
+                    isWorking = false;
+                    break;
+                }
+                counter = 0;
+                int numIngrd = System.Convert.ToInt32(Ingrd);
 
-            Console.WriteLine("\n--------ingredients--------");
-            recipe.ingediants = new List<Ingredient>();
-            for (int i = 0; i < numIngrd; i++)
-            {
-                Ingredient ingred = AddIngredient(i + 1);
-                recipe.ingediants.Add(ingred);
-            }
+                Console.WriteLine("\n--------ingredients--------");
+                recipe.ingediants = new List<Ingredient>();
+                for (int i = 0; i < numIngrd; i++)
+                {
+                    Ingredient ingred = AddIngredient(i + 1,ref isWorking);
+                    recipe.ingediants.Add(ingred);
+                }
 
 
-            Console.WriteLine("How many steps does this recipe require?");
-            string steps = Console.ReadLine();
-            isValid = InputChecker.IntChecker(steps);
-            while (isValid == false && counter < 2)
-            {
-                Console.WriteLine("Please Try again to enter the steps that this recipe require");
+                Console.WriteLine("How many steps does this recipe require?");
+                string steps = Console.ReadLine();
                 isValid = InputChecker.IntChecker(steps);
-                counter++;
-            }
-            if (isValid == false)
-            {
-                //break;
-            }
-            counter = 0;
-            int numSteps = System.Convert.ToInt32(steps);
+                while (isValid == false && counter < 2)
+                {
+                    Console.WriteLine("Please Try again to enter the steps that this recipe require");
+                    isValid = InputChecker.IntChecker(steps);
+                    counter++;
+                }
+                if (isValid == false)
+                {
+                    isWorking = false;
+                    break;
+                }
+                counter = 0;
+                int numSteps = System.Convert.ToInt32(steps);
 
-            Console.WriteLine("");
-            Console.WriteLine("\n--------Steps--------");
-            recipe.steps = new List<Step>();
-            for (int j = 0; j < numSteps; j++)
-            {
-                Step step = AddSteps(j + 1);
-                recipe.steps.Add(step);
+                Console.WriteLine("");
+                Console.WriteLine("\n--------Steps--------");
+                recipe.steps = new List<Step>();
+                for (int j = 0; j < numSteps; j++)
+                {
+                    Step step = AddSteps(j + 1, ref isWorking);
+                    recipe.steps.Add(step);
+                }
             }
-
             return recipe;
 
         }
 
-        static Ingredient AddIngredient(int x)
+        static Ingredient AddIngredient(int x, ref bool isWorking)
         {
             int counter = 0;
             bool isValid = false;
+            bool isChecked = true;
             Ingredient ingredient = new Ingredient();
 
-            Console.WriteLine($"What is the name of Ingredient No{x}?");
-            string name = Console.ReadLine();
-            isValid = InputChecker.StringChecker(name);
-            while (isValid == false && counter < 2)
+            while (isChecked == true)
             {
-                Console.WriteLine("Please Try again to enter the recipe name");
+                Console.WriteLine($"What is the name of Ingredient No{x}?");
+                string name = Console.ReadLine();
                 isValid = InputChecker.StringChecker(name);
-                counter++;
-            }
-            if (isValid == false)
-            {
-                //break;
-            }
-            counter = 0;
-            ingredient.name = name;
+                while (isValid == false && counter < 2)
+                {
+                    Console.WriteLine("Please Try again to enter the recipe name");
+                    isValid = InputChecker.StringChecker(name);
+                    counter++;
+                }
+                if (isValid == false)
+                {
+                    isWorking = false;
+                    isChecked = false;
+                    break;
+                }
+                counter = 0;
+                ingredient.name = name;
 
-            Console.WriteLine("What is the Unit of Measurement?");
-            string uom = Console.ReadLine();
-            isValid = InputChecker.StringChecker(uom);
-            while (isValid == false && counter < 2)
-            {
-                Console.WriteLine("Please Try again to enter the Unit of Measurement");
+                Console.WriteLine("What is the Unit of Measurement?");
+                string uom = Console.ReadLine();
                 isValid = InputChecker.StringChecker(uom);
-                counter++;
-            }
-            if (isValid == false)
-            {
-                //break;
-            }
-            counter = 0;
-            ingredient.UOM = uom;
+                while (isValid == false && counter < 2)
+                {
+                    Console.WriteLine("Please Try again to enter the Unit of Measurement");
+                    isValid = InputChecker.StringChecker(uom);
+                    counter++;
+                }
+                if (isValid == false)
+                {
+                    isWorking = false;
+                    isChecked = false;
+                    break;
+                }
+                counter = 0;
+                ingredient.UOM = uom;
 
-            Console.WriteLine("What is the quantity used");
-            string qt = Console.ReadLine();
-            isValid = InputChecker.IntChecker(qt);
-            while (isValid == false && counter < 2)
-            {
-                Console.WriteLine("Please Try again to enter the quantity used");
+                Console.WriteLine("What is the quantity used");
+                string qt = Console.ReadLine();
                 isValid = InputChecker.IntChecker(qt);
-                counter++;
+                while (isValid == false && counter < 2)
+                {
+                    Console.WriteLine("Please Try again to enter the quantity used");
+                    isValid = InputChecker.IntChecker(qt);
+                    counter++;
+                }
+                if (isValid == false)
+                {
+                    isWorking = false;
+                    isChecked = false;
+                    break;
+                }
+                counter = 0;
+                int Qnt = System.Convert.ToInt32(qt);
+                ingredient.Quantity = Qnt;
+                Console.WriteLine("----");
             }
-            if (isValid == false)
-            {
-                //break;
-            }
-            counter = 0;
-            int Qnt = System.Convert.ToInt32(qt);
-            ingredient.Quantity = Qnt;
-            Console.WriteLine("----");
-
             return ingredient;
         }
 
-        static Step AddSteps(int j)
+        static Step AddSteps(int j, ref bool isWorking)
         {
             int counter = 0;
             bool isValid = false;
+            bool isChecked = true;
             Step step = new Step();
 
-            step.step = j;
-            Console.WriteLine("Step" + j);
-            string thestep = Console.ReadLine();
-            isValid = InputChecker.StringChecker(thestep);
-            while (isValid == false && counter < 2)
+            while (isChecked == true)
             {
-                Console.WriteLine("Please Try again to enter the Unit of Measurement");
+                step.step = j;
+                Console.WriteLine("Step" + j);
+                string thestep = Console.ReadLine();
                 isValid = InputChecker.StringChecker(thestep);
-                counter++;
+                while (isValid == false && counter < 2)
+                {
+                    Console.WriteLine("Please Try again to enter the Unit of Measurement");
+                    isValid = InputChecker.StringChecker(thestep);
+                    counter++;
+                }
+                if (isValid == false)
+                {
+                    isWorking = false;
+                    isChecked = false;
+                    break;
+                }
+                counter = 0;
+                step.instruction = thestep;
+                Console.WriteLine("----");
             }
-            if (isValid == false)
-            {
-                //break;
-            }
-            counter = 0;
-            step.instruction = thestep;
-            Console.WriteLine("----");
             return step;
         }
     }
