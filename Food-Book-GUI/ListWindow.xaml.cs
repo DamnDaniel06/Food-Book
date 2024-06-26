91d3d2c6588b43623dab22a1b4fecd13cb469564
DamnDaniel06 <162707616+DamnDaniel06@users.noreply.github.com>
2024-06-26 11:08:03 +0200

    Updating Filtering

--- Food-Book-GUI/ListWindow.xaml.cs
+++ Food-Book-GUI/ListWindow.xaml.cs
@@ -61,7 +61,7 @@ namespace Food_Book_GUI
                         }
                     }
                 }
-            }
+            }//--->> finding a way to create action every time ComboBox changes
             //food type
             if (CbFilterby.SelectedIndex == 1)
             {//get selecte food group
@@ -98,6 +98,7 @@ namespace Food_Book_GUI
 
             if (newBook != null)
             {
+                HiddenStack.Visibility = Visibility.Hidden;
                 ObservableCollection<Recipe> recipes = new ObservableCollection<Recipe>();
                 foreach (var rept in newBook.recipes)
                 {
@@ -118,5 +119,36 @@ namespace Food_Book_GUI
                 //Do your stuff
             }
         }
+
+        private void CbFilterby_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CbFilterMust != null)
+            {
+                if (CbFilterby.SelectedIndex == 0)
+                {
+                    CbFilterMust.IsEnabled = true;
+                    UserInput.IsEnabled = true;
+                    ListFood.IsEnabled = false;
+                    CbFilterMinMax.IsEnabled = false;
+                }
+                else if (CbFilterby.SelectedIndex == 1)
+                {
+                    CbFilterMust.IsEnabled = true;
+                    CbFilterMinMax.IsEnabled = false;
+                    UserInput.IsEnabled = false;
+                    ListFood.IsEnabled = true;
+                }
+                else
+                {
+                    CbFilterMust.IsEnabled = false;
+                    ListFood.IsEnabled = false;
+                    CbFilterMinMax.IsEnabled = true;
+                    UserInput.IsEnabled = true;
+                }
+            }
+            
+            
+        }
+        public IEnumerable<string> choosing = new List<string> { "Ingredients", "Food Group", "Categories" };
     }
 }
